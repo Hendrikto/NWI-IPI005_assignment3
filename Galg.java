0054f15f153223c14e10844d9fc5c3755558313a
Hendrikto <hendrik.to@gmail.com>
2016-02-19 16:07:52 +0100

    Convert word to lower case before saving it.

--- Galg.java
+++ Galg.java
@@ -22,7 +22,7 @@ public class Galg {
      * @param allowedFails Maximum number of failed guesses.
      */
     public Galg (String word, int allowedFails) {
-        this.word = word;
+        this.word = word.toLowerCase();
         this.fails = 0;
         this.failThreshold = allowedFails;
         this.failedGuesses = "";
